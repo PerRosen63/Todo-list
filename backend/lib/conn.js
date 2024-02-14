aa060562732a31eccec9e1ca9babc92cce3ead55
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-02-15 00:10:33 +0100

    Övning klar - Codealong

---
+++ backend/lib/conn.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection( {
+    host: 'localhost',
+    port: '8889',
+    user: 'todofed23',
+    password: 'todofed23',
+    database: 'todofed23'
+})
+
+module.exports = connection; 
